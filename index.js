acb36a3e157c23b3650a94569c2f95711b7ff644
Dhanush1919 <dhanush.venkataraman@gmail.com>
2020-03-04 18:07:27 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-// Import stylesheets
-import './style.css';
+var x1=10;
+var x2=5;
+function add(x1,x2)
+{
+  const a=x1+x2;
+  return a;
+}
+function sub(x1,x2)
+{
+  const b=x1-x2;
+  return b;
+}
+function mul(x1,x2)
+{
+  const c=x1*x2;
+  return c;
+}
+function div(x1,x2)
+{
+  const d=x1/x2;
+  return d;
+}
+function mod(x1,x2)
+{
+  const e=x1%x2;
+  return e;
+}
+function exp(x1,x2)
+{
+  const f=x1**x2;
+  return f;
+}
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
